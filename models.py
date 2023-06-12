1941c6085d90d8c34c0e7211ef49ddffa11bbea9
yassine <boudouriyassine@gmail.com>
2023-06-12 22:15:28 +0200

    Initial Commit

---
+++ models.py
@@ -0,0 +1,272 @@
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.hub import load_state_dict_from_url
+from torchvision.ops import StochasticDepth
+
+model_urls = {
+    "convnext_tiny_1k": "https://dl.fbaipublicfiles.com/convnext/convnext_tiny_1k_224_ema.pth",
+    "convnext_small_1k": "https://dl.fbaipublicfiles.com/convnext/convnext_small_1k_224_ema.pth",
+    "convnext_base_1k": "https://dl.fbaipublicfiles.com/convnext/convnext_base_1k_224_ema.pth",
+    "convnext_large_1k": "https://dl.fbaipublicfiles.com/convnext/convnext_large_1k_224_ema.pth",
+    "convnext_tiny_22k": "https://dl.fbaipublicfiles.com/convnext/convnext_tiny_22k_224.pth",
+    "convnext_small_22k": "https://dl.fbaipublicfiles.com/convnext/convnext_small_22k_224.pth",
+    "convnext_base_22k": "https://dl.fbaipublicfiles.com/convnext/convnext_base_22k_224.pth",
+    "convnext_large_22k": "https://dl.fbaipublicfiles.com/convnext/convnext_large_22k_224.pth",
+    "convnext_xlarge_22k": "https://dl.fbaipublicfiles.com/convnext/convnext_xlarge_22k_224.pth",
+}
+
+
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
+
+class DotProductSelfAttention(nn.Module):
+    def __init__(self, input_dim):
+        super(DotProductSelfAttention, self).__init__()
+        self.input_dim = input_dim
+        self.norm = nn.LayerNorm(input_dim)
+        self.query = nn.Linear(input_dim, input_dim)
+        self.key = nn.Linear(input_dim, input_dim)
+        self.value = nn.Linear(input_dim, input_dim)
+
+    def forward(self, x):
+        x = self.norm(x)
+        query = self.query(x)
+        key = self.key(x)
+        value = self.value(x)
+
+        scale = 1 / math.sqrt(math.sqrt(self.input_dim))
+        scores = torch.matmul(query, key.transpose(-2, -1)) * scale
+        attention_weights = torch.softmax(scores, dim=-1)
+
+        attended_values = torch.matmul(attention_weights, value)
+        output = attended_values + x
+
+        return output, attention_weights
+
+
+class LayerNorm(nn.Module):
+    r""" LayerNorm that supports two data formats: channels_last (default) or channels_first.
+    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with
+    shape (batch_size, height, width, channels) while channels_first corresponds to inputs
+    with shape (batch_size, channels, height, width).
+    """
+
+    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError
+        self.normalized_shape = (normalized_shape,)
+
+    def forward(self, x):
+        if self.data_format == "channels_last":
+            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        elif self.data_format == "channels_first":
+            u = x.mean(1, keepdim=True)
+            s = (x - u).pow(2).mean(1, keepdim=True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+            return x
+
+
+class Block(nn.Module):
+    r""" ConvNeXt Block. There are two equivalent implementations:
+    (1) DwConv -> LayerNorm (channels_first) -> 1x1 Conv -> GELU -> 1x1 Conv; all in (N, C, H, W)
+    (2) DwConv -> Permute to (N, H, W, C); LayerNorm (channels_last) -> Linear -> GELU -> Linear; Permute back
+    We use (2) as we find it slightly faster in PyTorch
+
+    Args:
+        dim (int): Number of input channels.
+        drop_path (float): Stochastic depth rate. Default: 0.0
+        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
+    """
+
+    def __init__(self, dim, drop_path=0., layer_scale_init_value=1e-6):
+        super().__init__()
+        self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim)  # depthwise conv
+        self.norm = LayerNorm(dim, eps=1e-6)
+        self.pwconv1 = nn.Linear(dim, 4 * dim)  # pointwise/1x1 convs, implemented with linear layers
+        self.act = nn.GELU()
+        self.pwconv2 = nn.Linear(4 * dim, dim)
+        self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)),
+                                  requires_grad=True) if layer_scale_init_value > 0 else None
+        self.stochastic_depth = StochasticDepth(drop_path, "row")
+
+    def forward(self, x):
+        input = x
+        x = self.dwconv(x)
+        x = x.permute(0, 2, 3, 1)  # (N, C, H, W) -> (N, H, W, C)
+        x = self.norm(x)
+        x = self.pwconv1(x)
+        x = self.act(x)
+        x = self.pwconv2(x)
+        if self.gamma is not None:
+            x = self.gamma * x
+        x = x.permute(0, 3, 1, 2)  # (N, H, W, C) -> (N, C, H, W)
+
+        x = input + self.stochastic_depth(x)
+        return x
+
+
+class ConvNeXt(nn.Module):
+
+    def __init__(self, in_chans=3, num_classes=1000,
+                 depths=None, dims=None, drop_path_rate=0.,
+                 layer_scale_init_value=1e-6,
+                 ):
+        super().__init__()
+
+        if dims is None:
+            dims = [96, 192, 384, 768]
+        if depths is None:
+            depths = [3, 3, 9, 3]
+
+        # Spatial transformer localization-network
+        self.localization = nn.Sequential(
+            nn.Conv2d(3, 8, kernel_size=7),
+            nn.BatchNorm2d(8),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True),
+            nn.Conv2d(8, 10, kernel_size=5),
+            nn.BatchNorm2d(10),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True)
+        )
+
+        # Regressor for the 3 * 2 affine matrix
+        self.fc_loc = nn.Sequential(
+            nn.Linear(10 * 52 * 52, 32),
+            nn.ReLU(True),
+            nn.Linear(32, 3 * 2)
+        )
+
+        self.downsample_layers = nn.ModuleList()  # stem and 3 intermediate downsampling conv layers
+        stem = nn.Sequential(
+            nn.Conv2d(in_chans, dims[0], kernel_size=4, stride=4),
+            LayerNorm(dims[0], eps=1e-6, data_format="channels_first")
+        )
+        self.downsample_layers.append(stem)
+        for i in range(3):
+            downsample_layer = nn.Sequential(
+                LayerNorm(dims[i], eps=1e-6, data_format="channels_first"),
+                nn.Conv2d(dims[i], dims[i + 1], kernel_size=2, stride=2),
+                SELayer(dims[i + 1])
+            )
+            self.downsample_layers.append(downsample_layer)
+
+        self.stages = nn.ModuleList()  # 4 feature resolution stages, each consisting of multiple residual blocks
+        dp_rates = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]
+        cur = 0
+        for i in range(4):
+            stage = nn.Sequential(
+                *[Block(dim=dims[i], drop_path=dp_rates[cur + j],
+                        layer_scale_init_value=layer_scale_init_value) for j in range(depths[i])]
+            )
+            self.stages.append(stage)
+            cur += depths[i]
+
+        self.norm = nn.LayerNorm(dims[-1], eps=1e-6)  # final norm layer
+        self.attention = DotProductSelfAttention(dims[-1])
+        self.head = nn.Linear(dims[-1], num_classes)
+
+        for m in self.modules():
+            if isinstance(m, (nn.Conv2d, nn.Linear)):
+                nn.init.trunc_normal_(m.weight, std=0.02)
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+
+        self.fc_loc[2].weight.data.zero_()
+        self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+
+    def stn(self, x):
+        xs = self.localization(x)
+        xs = xs.view(-1, 10 * 52 * 52)
+        theta = self.fc_loc(xs)
+        theta = theta.view(-1, 2, 3)
+
+        grid = F.affine_grid(theta, x.size(), align_corners=True)
+        x = F.grid_sample(x, grid, align_corners=True)
+
+        return x
+
+    def forward_features(self, x):
+        for i in range(4):
+            x = self.downsample_layers[i](x)
+            x = self.stages[i](x)
+        return self.norm(x.mean([-2, -1]))  # global average pooling, (N, C, H, W) -> (N, C)
+
+    def forward(self, x, labels=None):
+        x = self.stn(x)
+        x = self.forward_features(x)
+        _, weights = self.attention(x)
+        logits = self.head(x)
+
+        if labels is not None:
+            mean_attention_weight = torch.mean(weights)
+            attention_loss = torch.mean((weights - mean_attention_weight) ** 2)
+
+            loss = F.cross_entropy(logits, labels, label_smoothing=0.2) + attention_loss
+            return torch.argmax(logits, dim=1), logits, loss
+
+        return torch.argmax(logits, dim=1), logits
+
+
+def get_model(num_classes, model_size='tiny', in_22k=False):
+    if model_size == 'tiny':
+        depths = [3, 3, 9, 3]
+        dims = [96, 192, 384, 768]
+        url = model_urls['convnext_tiny_22k'] if in_22k else model_urls['convnext_tiny_1k']
+    elif model_size == 'small':
+        depths = [3, 3, 27, 3]
+        dims = [96, 192, 384, 768]
+        url = model_urls['convnext_small_22k'] if in_22k else model_urls['convnext_small_1k']
+    elif model_size == 'base':
+        depths = [3, 3, 27, 3]
+        dims = [128, 256, 512, 1024]
+        url = model_urls['convnext_base_22k'] if in_22k else model_urls['convnext_base_1k']
+    elif model_size == 'large':
+        depths = [3, 3, 27, 3]
+        dims = [192, 384, 768, 1536]
+        url = model_urls['convnext_large_22k'] if in_22k else model_urls['convnext_large_1k']
+    else:
+        depths = [3, 3, 27, 3]
+        dims = [256, 512, 1024, 2048]
+        url = model_urls['convnext_xlarge_22k']
+
+    default_num_classes = 1000
+    if in_22k:
+        default_num_classes = 21841
+
+    net = ConvNeXt(
+        depths=depths,
+        dims=dims,
+        num_classes=default_num_classes,
+        drop_path_rate=0.1
+    )
+
+    state_dict = load_state_dict_from_url(url=url)
+    net.load_state_dict(state_dict['model'], strict=False)
+    net.head = nn.Linear(dims[-1], num_classes)
+
+    return net
